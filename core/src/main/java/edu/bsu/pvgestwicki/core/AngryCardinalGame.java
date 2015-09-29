025eec3b6c3542c02d2eaea69683421cc5469bb3
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-09-29 08:43:02 -0400

    Show Charlie
    Charlie is added as an imagelayer to the bottom left corner of the screen.

--- core/src/main/java/edu/bsu/pvgestwicki/core/AngryCardinalGame.java
+++ core/src/main/java/edu/bsu/pvgestwicki/core/AngryCardinalGame.java
@@ -3,18 +3,29 @@ package edu.bsu.pvgestwicki.core;
 import playn.core.Image;
 import playn.core.Platform;
 import playn.scene.ImageLayer;
+import playn.scene.Layer;
 import playn.scene.SceneGame;
 
 public class AngryCardinalGame extends SceneGame {
 
     public AngryCardinalGame(Platform plat) {
         super(plat, 33); // update our "simulation" 33ms (30 times per second)
+        initBackground();
+        initCharlie();
+    }
 
-        // create and add background image layer
+    private void initBackground() {
         Image bgImage = plat.assets().getImage("images/bg.png");
         ImageLayer bgLayer = new ImageLayer(bgImage);
-        // scale the background to fill the screen
         bgLayer.setSize(plat.graphics().viewSize);
         rootLayer.add(bgLayer);
+    }
+
+    private void initCharlie() {
+        Image charlieImage = plat.assets().getImageSync("images/charlie.png");
+        ImageLayer charlieLayer = new ImageLayer(charlieImage);
+        charlieLayer.setOrigin(Layer.Origin.LL);
+        charlieLayer.setTy(plat.graphics().viewSize.height());
+        rootLayer.add(charlieLayer);
     }
 }
